f36d6ae391273b26b8778ff25e1b0247e176cd6b
Brendon Thiede <brendon.thiede@gmail.com>
2012-02-20 13:34:17 -0500

    Added in support for current and failed builds.  Required removal of time check

--- bamboo.php
+++ bamboo.php
@@ -1,27 +1,19 @@
 <div id="bambooPlans">
 <?php
+	include('includes/common.inc');
 	$cfg = fopen (dirname(__FILE__)."\\config\\bambooPlanList.cfg", "r");
 	$index = 0;
 	while ($url = trim(fgets($cfg))) {
+		$domain = getFirstRegexCapture('/(http[s]?:\/\/[^\/]*)\//', $url);
 		$buildStatusPage = file_get_contents($url);
 		$pattern = '/<div id="buildStatusInner3"> <p class="([^"]*)"> <strong>Latest Status:<\/strong> (.*) <\/p> <\/div> <!-- END #buildStatusInner3 -->/';
 		if (preg_match($pattern, $buildStatusPage, $matches)) {
 			$buildStatus = $matches[1];
-			$statusSummary = $matches[2];
-			$buildCompletionTime = "";
-			$pattern = '/Build completed on <strong>(.*)<\/strong>/';
-			if (preg_match($pattern, $buildStatusPage, $matches)) {
-				$buildCompletionTime = $matches[1];
-			}
+			$statusSummary = str_replace('<a href="/', '<a href="'.$domain.'/', $matches[2]);
 			
-			echo("\t<div id='bambooPlan".$index."' class='".$buildStatus."'>");
-			if ($buildStatus == "success") {
-				echo("<img alt='success' src='http://10.6.0.145:8035/s/1600/1/_/images/jt/icn_plan_passed.gif'> ");
-			}
-			echo($statusSummary);
-			echo(" at <span id='buildCompletionTime".$index."' class='buildCompletionTime'>".$buildCompletionTime."</span>");
-			echo("</div>\n");
-			$index++;
+			echo("\t<div id='bambooPlan".$index++."' class='".$buildStatus."'>");
+			echo("<img alt='".$buildStatus."' src='images/".$buildStatus.".gif' />");
+			echo(" ".$statusSummary."</div>\n");
 		}
 	}
 	fclose ($cfg);
