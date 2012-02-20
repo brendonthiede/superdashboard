335cc8ac4b34c36b07b64db6d0de5eba151af49c
unknown <Brendon@.(none)>
2012-02-19 21:26:49 -0500

    Currently when an error is triggered, displays image and plays a sound

--- js/base.js
+++ js/base.js
@@ -4,16 +4,125 @@
  */
 
 var superdashboard = (function() {
-	var isInitialized = false;
-	
-	var init = function() {
-		isInitialized = true;
-	};
-	
 	var alerts = (function() {
+		var isAlertShowing = false;
+		var audioCount = 0;
+
 		var isAlertExists = function() {
-			
+			return $("#buildError").is(":checked");
+		};
+
+		var checkForAlert = function() {
+			if (isAlertExists()) {
+				if (!isAlertShowing) {
+					$("#sirenDiv").show();
+					isAlertShowing = true;
+					playSound("audio/pchick-alarm.wav", 4);
+				}
+			} else {
+				if (isAlertShowing) {
+					$("#sirenDiv").hide();
+					isAlertShowing = false;
+				}
+			}
+			setTimeout("superdashboard.alerts.checkForAlert();", 2000);
+		}
+		
+		var playSound = function(url, count) {
+//			for (var i = 0; i < count; i++) {
+				//$("#sound").html("<embed type='audio/x-wav' src='"+url+"' hidden=true autostart=true loop=false />");
+				document.getElementById('alarmAudioPlayer').play();
+//			}
+		};
+		
+		var snooze = function() {
+			$("#sirenDiv").hide();
+		};
+
+		return {
+			checkForAlert: checkForAlert,
+			snooze: snooze
 		};
 	})();
-})();	
 
+	var slider = (function() {
+		var isInitialized = false;
+		var sliderTimer = null;
+		var isAutoSlide = false;
+		var autoSlideDuration = 5000;
+		var sliderIndex = 0;
+		var sliderCount = 0;
+		
+		var init = function() {
+			sliderCount = $("div.slide").length;
+			sliderIndex = sliderCount - 1;
+			isInitialized = true;
+		};
+		
+		var transitionForward = function() {
+			transitionSlide(sliderIndex + 1);
+		};
+		
+		var transitionBackward = function() {
+			transitionSlide(sliderIndex - 1);
+		};
+		
+		var transitionSlide = function(index) {
+			$("div.slide").css('z-index', 10);
+			$("div.slide").eq(sliderIndex).css('z-index', 20);
+			if (index >= sliderCount) {
+				sliderIndex = 0;
+			} else if (index < 0) {
+				sliderIndex = sliderCount - 1;
+			} else {
+				sliderIndex = index;
+			}
+			$("#sliderCount").text((sliderIndex + 1) + '/' + sliderCount);
+			$("div.slide").eq(sliderIndex).css('height', '0').css('z-index', 30).animate({height: '100%'}, 1500);
+		};
+		
+		var startSlider = function(duration) {
+			isAutoSlide = true;
+			if (duration > 1500) {
+				autoSlideDuration = duration;
+			}
+			showNext();
+			$("#startStopSlider").html("<a href='#' onClick='superdashboard.slider.stopSlider();'>Manual</a>");
+		};
+		
+		var stopSlider = function() {
+			clearTimeout(sliderTimer);
+			isAutoSlide = false;
+			$("#startStopSlider").html("<a href='#' onClick='superdashboard.slider.startSlider();'>Auto</a>");
+		};
+		
+		var showNext = function() {
+			if (!isInitialized) {
+				init();
+			}
+			transitionForward();
+			if (isAutoSlide) {
+				sliderTimer = setTimeout("superdashboard.slider.showNext();", autoSlideDuration);
+			}
+		};
+		
+		var showPrevious = function() {
+			if (!isInitialized) {
+				init();
+			}
+			transitionBackward();
+		};
+		
+		return {
+			startSlider: startSlider,
+			stopSlider: stopSlider,
+			showNext: showNext,
+			showPrevious: showPrevious
+		};
+	})();
+
+	return {
+		alerts: alerts,
+		slider: slider
+	};	
+})();
