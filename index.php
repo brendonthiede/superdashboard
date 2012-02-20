7ba4c7426bcb0463341ae84823f0169b84e13846
unknown <Brendon@.(none)>
2012-02-19 22:42:47 -0500

    Style changes to function declarations and switched some spaces for tabs (I'd prefer the other way around, but this was the minority using Aptana)

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <html>
-    <head>
-        <title>Super Dashboard</title>
-        <link href="css/base.css" rel="stylesheet" type="text/css" />
-        <script type="text/javascript" src="js/jquery1.7.1.min.js"></script>
-        <script type="text/javascript" src="js/base.js"></script>
-        <script type="text/javascript" src="js/alerts.js"></script>
-        <script type="text/javascript" src="js/slider.js"></script>
-        <script type="text/javascript">
-        	$(document).ready(function() {
-        		superdashboard.alerts.checkForAlert();
-        		superdashboard.slider.showNext();
-        	});
-        </script>
-    </head>
-    <body>
-    	<?php include("includes/slider.inc"); ?>
-        <?php include("includes/alarmAudio.inc"); ?>
-        <?php include("includes/alarmVisual.inc"); ?>
+	<head>
+		<title>Super Dashboard</title>
+		<link href="css/base.css" rel="stylesheet" type="text/css" />
+		<script type="text/javascript" src="js/jquery1.7.1.min.js"></script>
+		<script type="text/javascript" src="js/base.js"></script>
+		<script type="text/javascript" src="js/alerts.js"></script>
+		<script type="text/javascript" src="js/slider.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				superdashboard.alerts.checkForAlert();
+				superdashboard.slider.showNext();
+			});
+		</script>
+	</head>
+	<body>
+		<?php include("includes/slider.inc"); ?>
+		<?php include("includes/alarmAudio.inc"); ?>
+		<?php include("includes/alarmVisual.inc"); ?>
 		<!-- Temporary checkbox for testing out alarm capabilities -->
-        <div id="systemStatusDiv" style="position: relative; top: 0; left: 0;"><input id="buildError" type="checkbox" />Build Error</div>
-    </body>
+		<div id="systemStatusDiv" style="position: relative; top: 0; left: 0;"><input id="buildError" type="checkbox" />Build Error</div>
+	</body>
 </html>
--- js/slider.js
+++ js/slider.js
@@ -6,21 +6,22 @@ superdashboard.slider = (function() {
 	var sliderIndex = 0;
 	var sliderCount = 0;
 	
-	var init = function() {
+	// private functions
+	function init() {
 		sliderCount = $("div.slide").length;
 		sliderIndex = sliderCount - 1;
 		isInitialized = true;
-	};
+	}
 	
-	var transitionForward = function() {
+	function transitionForward() {
 		transitionSlide(sliderIndex + 1);
-	};
+	}
 	
-	var transitionBackward = function() {
+	function transitionBackward() {
 		transitionSlide(sliderIndex - 1);
-	};
+	}
 	
-	var transitionSlide = function(index) {
+	function transitionSlide(index) {
 		$("div.slide").css('z-index', 10);
 		$("div.slide").eq(sliderIndex).css('z-index', 20);
 		if (index >= sliderCount) {
@@ -32,24 +33,25 @@ superdashboard.slider = (function() {
 		}
 		$("#sliderCount").text((sliderIndex + 1) + '/' + sliderCount);
 		$("div.slide").eq(sliderIndex).css('height', '0').css('z-index', 30).animate({height: '100%'}, 1500);
-	};
+	}
 	
-	var startSlider = function(duration) {
+	// public functions
+	function startSlider(duration) {
 		isAutoSlide = true;
 		if (duration > 1500) {
 			autoSlideDuration = duration;
 		}
 		showNext();
 		$("#startStopSlider").html("<a href='#' onClick='superdashboard.slider.stopSlider();'>Manual</a>");
-	};
+	}
 	
-	var stopSlider = function() {
+	function stopSlider() {
 		clearTimeout(sliderTimer);
 		isAutoSlide = false;
 		$("#startStopSlider").html("<a href='#' onClick='superdashboard.slider.startSlider();'>Auto</a>");
-	};
+	}
 	
-	var showNext = function() {
+	function showNext() {
 		if (!isInitialized) {
 			init();
 		}
@@ -57,14 +59,14 @@ superdashboard.slider = (function() {
 		if (isAutoSlide) {
 			sliderTimer = setTimeout("superdashboard.slider.showNext();", autoSlideDuration);
 		}
-	};
+	}
 	
-	var showPrevious = function() {
+	function showPrevious() {
 		if (!isInitialized) {
 			init();
 		}
 		transitionBackward();
-	};
+	}
 	
 	return {
 		startSlider: startSlider,
