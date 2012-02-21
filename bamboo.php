f17fb2a7563bdb74c09ad53fea6931ad9374fe00
Brendon Thiede <brendon.thiede@gmail.com>
2012-02-20 21:31:05 -0500

    Added error handling to bamboo.php to avoid wasting time try to connect to unavailable resources

--- bamboo.php
+++ bamboo.php
@@ -2,17 +2,27 @@
 	include('includes/common.inc');
 	$cfg = fopen (dirname(__FILE__)."\\config\\bambooPlanList.cfg", "r");
 	$index = 0;
+	$badDomains = array();
 	while ($url = trim(fgets($cfg))) {
 		$domain = getFirstRegexCapture('/(http[s]?:\/\/[^\/]*)\//', $url);
-		$buildStatusPage = file_get_contents($url);
-		$pattern = '/<div id="buildStatusInner3"> <p class="([^"]*)"> <strong>Latest Status:<\/strong> (.*) <\/p> <\/div> <!-- END #buildStatusInner3 -->/';
-		if (preg_match($pattern, $buildStatusPage, $matches)) {
-			$buildStatus = $matches[1];
-			$statusSummary = str_replace('<a href="/', '<a href="'.$domain.'/', $matches[2]);
-			
-			echo("\t<div id='bambooPlan".$index++."' class='".$buildStatus."'>");
-			echo("<img alt='".$buildStatus."' src='images/".$buildStatus.".gif' />");
-			echo(" ".$statusSummary."</div>\n");
+		if (!isset($badDomains[$domain])) {
+			$buildStatusPage = @file_get_contents($url);
+			if ($buildStatusPage === false) {
+				echo "<div class='bambooError'>";
+				echo "Error loading ".$url." (".$domain." will now be ignored).";
+				echo "</div>";
+				$badDomains[$domain] = $domain;
+			} else {
+				$pattern = '/<div id="buildStatusInner3"> <p class="([^"]*)"> <strong>Latest Status:<\/strong> (.*) <\/p> <\/div> <!-- END #buildStatusInner3 -->/';
+				if (preg_match($pattern, $buildStatusPage, $matches)) {
+					$buildStatus = $matches[1];
+					$statusSummary = str_replace('<a href="/', '<a href="'.$domain.'/', $matches[2]);
+					
+					echo("\t<div id='bambooPlan".$index++."' class='".$buildStatus."'>");
+					echo("<img alt='".$buildStatus."' src='images/".$buildStatus.".gif' />");
+					echo(" ".$statusSummary."</div>\n");
+				}
+			}
 		}
 	}
 	fclose ($cfg);
